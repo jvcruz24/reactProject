f3ed38e5b7c30d83f65c768cf2dc3c0eb70cdce6
JV <jvcruz1998@gmail.com>
2022-06-05 12:29:53 +0800

    Checkout Features

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,14 +3,20 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
+import { useNavigate } from "react-router-dom";
 const CartDropdown = () =>{
     const {cartItems} = useContext(CartContext);
+    const navigate = useNavigate();
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    };
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
                 {cartItems.map(item => <CartItem key={item.id} cartItem={item}></CartItem>)}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+
         </div>
     )
 }
